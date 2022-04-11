a3b7d9b272f9b7b1ac6f13a7336f7fb5e23ec7b2
MChrisGM <c.i.cristakos@gmail.com>
2022-04-11 01:04:57 +0100

    Added card shuffling

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from os import listdir
 from os.path import isfile, join
 import cv2
+import random
 
 cards_table = {
     '2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'jack':11,'queen':12,'king':13,'ace':14,'clubs':1,'clubs2':0,'diamonds':2,'diamonds2':0,'hearts':3,'hearts2':0,'spades':4,'spades2':0,
@@ -12,19 +13,16 @@ class Card:
         self.value, self.face, self.face_name = self.calculate_value(filename)
         self.png = cv2.imread(cards_dir+'/'+filename)
         return
-    
     def calculate_value(self, filename):
         n = filename.split("_")
         v = cards_table[str(n[0])]
         f = cards_table[str(n[2].split('.')[0])]
         fn = str(n[2].split('.')[0])
         return v, f, fn
-
     def show(self, pos):
         cv2.namedWindow(str(self))
         cv2.moveWindow(str(self), pos[0], pos[1])
         cv2.imshow(str(self), self.png)
-    
     def __str__(self):
         return str(self.value) + " of "+str(self.face_name)
 
@@ -39,14 +37,17 @@ class Deck:
         return
     def get_cards(self):
         return self.cards
+    def show(self, pos):
+        for card in self.get_cards():
+            card.show(pos=pos)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+    def shuffle(self):
+        random.shuffle(self.cards)
 
 if __name__=="__main__":
-    
     pngs = [(str(f)) for f in listdir(cards_dir) if isfile(join(cards_dir, f))]
-
     deck = Deck(pngs)
-
-    for card in deck.get_cards():
-        card.show(pos=(0, 0))
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+    deck.shuffle()
+    deck.show(pos=(0, 0))
+    
